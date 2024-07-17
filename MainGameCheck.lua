23b8367e48bac919affd343b6b99122e05d99f36
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-07-17 14:46:15 +0700

    Update MainGameCheck.lua

--- MainGameCheck.lua
+++ MainGameCheck.lua
@@ -70,6 +70,8 @@ if getgenv().ScriptName ~= nil then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/NewCMain/main/Script/TheMimic.lua"))();
     elseif getgenv().ScriptName == "FTF" then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/NewCMain/main/Script/FTF.lua"))();
+    elseif getgenv().ScriptName == "UTS" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/NewCMain/main/Script/UTS.lua"))();
     end;
 else
     game:GetService("Players").LocalPlayer:Kick("Unsupported Experience");
